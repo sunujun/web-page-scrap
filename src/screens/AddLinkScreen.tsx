1d07e363568966da44705a2725daaae946c66d4c
seongjun.oh <tiger8708@gmail.com>
2023-02-26 08:55:24 +0900

    로딩 기능 추가

--- src/screens/AddLinkScreen.tsx
+++ src/screens/AddLinkScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useWindowDimensions, View } from 'react-native';
+import { ActivityIndicator, useWindowDimensions, View } from 'react-native';
 import { useSetRecoilState } from 'recoil';
 import { useNavigation } from '@react-navigation/native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
@@ -26,6 +26,7 @@ export const AddLinkScreen = () => {
     const { width } = useWindowDimensions();
     const [url, setUrl] = useState('');
     const [metadata, setMetadata] = useState<Metadata | null>(null);
+    const [loading, setLoading] = useState(false);
     const onPressClose = () => {
         navigation.goBack();
     };
@@ -50,8 +51,10 @@ export const AddLinkScreen = () => {
         setUrl('');
     };
     const onSubmitEditing = async () => {
+        setLoading(true);
         const result = await getOpenGraphData(url);
         setMetadata(result);
+        setLoading(false);
     };
     const onGetClipboardString = async () => {
         const result = await getClipboardString();
@@ -81,7 +84,6 @@ export const AddLinkScreen = () => {
             <View
                 style={{
                     flex: 1,
-                    alignItems: 'center',
                     justifyContent: 'flex-start',
                     paddingTop: 32,
                     paddingHorizontal: 24,
@@ -92,23 +94,52 @@ export const AddLinkScreen = () => {
                     onSubmitEditing={onSubmitEditing}
                     placeholder="https://example.com"
                 />
-                {metadata !== null && (
+                {loading ? (
                     <>
                         <Spacer space={20} />
                         <View style={{ borderWidth: 1, borderRadius: 4, borderColor: 'gray' }}>
-                            <RemoteImage url={metadata.image} width={width - 48} height={(width - 48) / 2} />
-                            <View style={{ paddingHorizontal: 12, paddingVertical: 8 }}>
-                                <Spacer space={10} />
-                                <Typography fontSize={20} color="black">
-                                    {metadata.title}
-                                </Typography>
-                                <Spacer space={4} />
-                                <Typography fontSize={16} color="gray">
-                                    {metadata.description}
-                                </Typography>
+                            <Spacer space={(width - 48) * 0.5} />
+                            <Spacer space={50} />
+                            <View
+                                style={{
+                                    position: 'absolute',
+                                    left: 0,
+                                    right: 0,
+                                    top: 0,
+                                    bottom: 0,
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                }}>
+                                <ActivityIndicator />
                             </View>
                         </View>
                     </>
+                ) : (
+                    metadata !== null && (
+                        <>
+                            <Spacer space={20} />
+                            <View style={{ borderWidth: 1, borderRadius: 4, borderColor: 'gray' }}>
+                                <RemoteImage
+                                    url={metadata.image}
+                                    width={width - 52}
+                                    height={(width - 52) / 2}
+                                    style={{
+                                        marginTop: 4,
+                                    }}
+                                />
+                                <View style={{ paddingHorizontal: 12, paddingVertical: 8 }}>
+                                    <Spacer space={10} />
+                                    <Typography fontSize={20} color="black">
+                                        {metadata.title}
+                                    </Typography>
+                                    <Spacer space={4} />
+                                    <Typography fontSize={16} color="gray">
+                                        {metadata.description}
+                                    </Typography>
+                                </View>
+                            </View>
+                        </>
+                    )
                 )}
             </View>
             <Button onPress={onPressSave}>
