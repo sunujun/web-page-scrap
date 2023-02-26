cf3a925d9a2ed63acf023d7716500110ee5d8ec0
seongjun.oh <tiger8708@gmail.com>
2023-02-26 09:16:50 +0900

    날짜, 시간별 SectionList로 변경

--- src/screens/LinkListScreen.tsx
+++ src/screens/LinkListScreen.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, FlatList } from 'react-native';
+import React, { useMemo } from 'react';
+import { View, SectionList } from 'react-native';
 import { useRecoilValue } from 'recoil';
 import { useNavigation } from '@react-navigation/native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
@@ -22,6 +22,42 @@ export const LinkListScreen = () => {
         navigation.navigate('AddLink');
     };
 
+    type dateListType = {
+        [dateId: string]: LinkItem[];
+    };
+
+    const sectionData = useMemo(() => {
+        const dateList: dateListType = {};
+
+        const makeDateString = (createdAt: string) => {
+            const dateItem = new Date(createdAt);
+
+            return `${dateItem.getFullYear()}.${
+                dateItem.getMonth() + 1
+            }.${dateItem.getDate()} ${dateItem.getHours()}:${dateItem.getMinutes()}`;
+        };
+
+        if (!data.list) {
+            return [];
+        }
+
+        data.list.forEach(item => {
+            const keyName = makeDateString(item.createdAt);
+            if (!dateList[keyName]) {
+                dateList[keyName] = [item];
+            } else {
+                dateList[keyName].push(item);
+            }
+        });
+
+        return Object.keys(dateList).map(item => {
+            return {
+                title: item,
+                data: dateList[item],
+            };
+        });
+    }, [data.list]);
+
     return (
         <View style={{ flex: 1 }}>
             <Header>
@@ -29,26 +65,32 @@ export const LinkListScreen = () => {
                     <Header.Title title="LINK LIST" />
                 </Header.Group>
             </Header>
-            <FlatList
+            <SectionList
                 style={{ flex: 1 }}
-                data={data.list}
+                sections={sectionData}
                 renderItem={({ item }) => {
                     return (
-                        <Button
-                            onPress={() => {
-                                onPressListItem(item);
-                            }}
-                            paddingHorizontal={24}
-                            paddingVertical={24}>
+                        <Button onPress={() => onPressListItem(item)} paddingHorizontal={24} paddingVertical={24}>
                             <View>
                                 <Typography fontSize={20}>{item.link}</Typography>
+
                                 <Spacer space={4} />
+
                                 <Typography fontSize={16} color="gray">
-                                    {item.title !== '' ? item.title.slice(0, 20) + ' | ' : ''}
+                                    {item.title !== '' ? `${item.title.slice(0, 20)} | ` : ''}
                                     {new Date(item.createdAt).toLocaleString()}
                                 </Typography>
                             </View>
                         </Button>
+                    );
+                }}
+                renderSectionHeader={({ section }) => {
+                    return (
+                        <View style={{ paddingHorizontal: 12, paddingVertical: 4, backgroundColor: 'white' }}>
+                            <Typography color="gray" fontSize={12}>
+                                {section.title}
+                            </Typography>
+                        </View>
                     );
                 }}
             />
